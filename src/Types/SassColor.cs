c44d6dfd523b8d663e0258237447266516043901
Adeel <adeelbm@outlook.com>
2016-05-08 13:36:05 +0300

    Comply with libsass rules

--- src/Types/SassColor.cs
+++ src/Types/SassColor.cs
@@ -4,16 +4,21 @@ namespace Sass.Types
 {
     public class SassColor : ISassType, ISassExportableType
     {
-        public double Red { get; set; } = 0;
-        public double Green { get; set; } = 0;
-        public double Blue { get; set; } = 0;
+        public int Red { get; set; } = 0;
+        public int Green { get; set; } = 0;
+        public int Blue { get; set; } = 0;
         public double Alpha { get; set; } = 1;
 
         private IntPtr CachedPtr { get; set; }
 
         public override string ToString()
         {
-            return $"{Alpha}{Red}{Green}{Blue}";
+            var red = Math.Min(Math.Max(Red, 0), 255);
+            var green = Math.Min(Math.Max(Green, 0), 255);
+            var blue = Math.Min(Math.Max(Blue, 0), 255);
+            var alpha = Math.Min(Math.Max(Alpha, 0), 1.0);
+
+            return $"rgba({red},{green},{blue},{alpha})";
         }
 
         IntPtr ISassExportableType.GetInternalTypePtr()
